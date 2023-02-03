d9f7807cd02e0f71b5a7a2788a6c16ea756449e0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-03 13:14:26 -0500

    small tweaks

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -13,15 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
 import { mount } from 'cypress/react18'
 
 Cypress.Commands.add('mount', mount)
-
-// Example use:
-// cy.mount(<MyComponent />)
